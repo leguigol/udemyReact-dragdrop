30eed1750fd0ecffc570e67346b409ec9dafa203
legui <leguigol@gmail.com>
2023-12-05 00:38:05 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import {DragDropContext, Droppable, Draggable} from '@hello-pangea/dnd'
 
-const initialTodos=[
+const initialTodos=JSON.parse(localStorage.getItem('todos')) || [
   {id: 1, text: 'Aprender React'},
   {id: 2, text: 'Aprender Js'},
   {id: 3, text: 'Aprender Vue.js'},
@@ -10,9 +10,26 @@ const initialTodos=[
 const App = () => { 
 
   const [todos, setTodos] = useState(initialTodos)
+
+  useEffect(()=>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+  },[todos])
+
+  const handleDragEnd=(result)=>{
+    if(!result.destination) return;
+    const startIndex=result.source.index
+    const endIndex=result.destination.index
+
+    const copyArray=[...todos]
+    const [reorderItem]=copyArray.splice(startIndex, 1)
+    console.log(reorderItem)
+    copyArray.splice(endIndex, 0, reorderItem)
+    console.log(copyArray)
+    setTodos(copyArray)
+  }
   return (
     <>
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <h1>Todo App</h1>
       <Droppable droppableId="todos">
         {
